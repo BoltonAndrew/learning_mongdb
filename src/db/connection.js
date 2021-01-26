c4fdfa3191614daec033868261b5e68e75a826eb
BostonWafro <wafrogaming@gmail.com>
2021-01-26 15:38:51 +0000

    set up mongoDB with mongoose

---
+++ src/db/connection.js
@@ -0,0 +1,14 @@
+/*jshint esversion: 8*/
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connection = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log('successfully connected to MongoDB');    
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+connection();
